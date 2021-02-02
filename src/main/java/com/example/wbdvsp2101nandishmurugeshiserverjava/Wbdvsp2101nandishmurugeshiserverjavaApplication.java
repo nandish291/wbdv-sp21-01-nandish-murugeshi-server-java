b45df55797c4bdee21f0da6ba811d4820da41d4a
Nandish Murugeshi <nandish291@live.com>
2021-02-01 22:50:03 -0500

    Project Created and login page created, system.properties added for heroku

---
+++ src/main/java/com/example/wbdvsp2101nandishmurugeshiserverjava/Wbdvsp2101nandishmurugeshiserverjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101nandishmurugeshiserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wbdvsp2101nandishmurugeshiserverjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wbdvsp2101nandishmurugeshiserverjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wbdvsp2101nandishmurugeshiserverjava/Wbdvsp2101nandishmurugeshiserverjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101nandishmurugeshiserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wbdvsp2101nandishmurugeshiserverjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
